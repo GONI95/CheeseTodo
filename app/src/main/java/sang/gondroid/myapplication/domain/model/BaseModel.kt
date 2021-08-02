3fe18dbb0f7acbc10a9504995d62679832bda71b
GONI95 <sanggoni009@gmail.com>
2021-08-03 04:07:24 +0900

    create baseModel, TodoModel

---
+++ app/src/main/java/sang/gondroid/myapplication/domain/model/BaseModel.kt
@@ -0,0 +1,45 @@
+package sang.gondroid.myapplication.domain.model
+
+import android.annotation.SuppressLint
+import android.graphics.ColorSpace
+import android.os.Parcel
+import android.os.Parcelable
+import android.util.Log
+import androidx.recyclerview.widget.DiffUtil
+import sang.gondroid.myapplication.util.Constants
+import java.io.Serializable
+
+ @SuppressLint("ParcelCreator")
+abstract class BaseModel (
+     open val id : Long?
+) : Serializable {
+     init {
+        Log.d(Constants.TAG, "Model $THIS_NAME called")
+    }
+
+    companion object {
+        private val THIS_NAME = this::class.simpleName
+
+        /**
+         * areItemsTheSame() : 두 객체가 동일한 항목을 나타내는지 확인
+         * areContesTheSame() : 두 항목의 데이터가 같은지 확인
+         */
+        val DIFF_CALLBACK : DiffUtil.ItemCallback<BaseModel> = object : DiffUtil.ItemCallback<BaseModel>() {
+            override fun areItemsTheSame(oldItem: BaseModel, newItem: BaseModel): Boolean {
+                Log.d(Constants.TAG, "${THIS_NAME} areItemsTheSame() called")
+
+                return oldItem.id == newItem.id
+                // id가 같아도 여러화면에서 쓰이면서 다른 데이터를 가져올 수 있어 타입까지 확인
+            }
+
+            @SuppressLint("DiffUtilEquals")
+            override fun areContentsTheSame(oldItem: BaseModel, newItem: BaseModel): Boolean {
+                Log.d(Constants.TAG, "${THIS_NAME} areContentsTheSame() called")
+
+                return oldItem === newItem
+                //같은 객체인지 비교하는 같은 내용이 들어간지 판단하기위해,
+            }
+
+        }
+    }
+ }
---
+++ app/src/main/java/sang/gondroid/myapplication/domain/model/TodoModel.kt
@@ -0,0 +1,23 @@
+package sang.gondroid.myapplication.domain.model
+
+import android.graphics.ColorSpace
+import android.os.Parcel
+import android.os.Parcelable
+import sang.gondroid.myapplication.data.entity.TodoEntity
+import sang.gondroid.myapplication.util.TodoCategory
+
+data class TodoModel(
+    override var id: Long,
+    val date: Long,
+    val dateString: String,
+    val category: TodoCategory,
+    val importanceId: Int,
+    val title: String,
+    val todo: String,
+    val difficult: String
+) : BaseModel(id) {
+
+    fun toEntity() = TodoEntity(
+        id, date, category, importanceId, title, todo, difficult
+    )
+}
