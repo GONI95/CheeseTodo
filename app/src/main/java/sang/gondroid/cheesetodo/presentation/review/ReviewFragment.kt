72e4356572e5d932895b7d108260b03da202504b
GONI95 <sanggoni009@gmail.com>
2021-08-29 20:43:54 +0900

    OnFocusChangeListener 구현 후 SearchView에 연결

--- app/src/main/java/sang/gondroid/cheesetodo/presentation/review/ReviewFragment.kt
+++ app/src/main/java/sang/gondroid/cheesetodo/presentation/review/ReviewFragment.kt
@@ -9,6 +9,9 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.net.toUri
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.databinding.adapters.SearchViewBindingAdapter.setOnQueryTextListener
 import androidx.lifecycle.Observer
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import io.reactivex.rxjava3.disposables.CompositeDisposable
@@ -34,14 +37,17 @@ import sang.gondroid.cheesetodo.widget.review.ReviewTodoListener
 
 @Suppress("UNCHECKED_CAST")
 class ReviewFragment  : BaseFragment<ReviewViewModel, FragmentReviewBinding>(),
-    androidx.appcompat.widget.SearchView.OnQueryTextListener {
+    androidx.appcompat.widget.SearchView.OnQueryTextListener,
+    View.OnFocusChangeListener {
     private val THIS_NAME = this::class.simpleName
 
     override val viewModel: ReviewViewModel by viewModel()
 
     private val liveSharedPreferences: LiveSharedPreferences by inject()
 
-    private lateinit var mSearchView: androidx.appcompat.widget.SearchView
+    private val mSearchView: androidx.appcompat.widget.SearchView by lazy {
+        binding.toolbar.menu.findItem(R.id.search_menu_item)?.actionView as androidx.appcompat.widget.SearchView
+    }
 
     private val reviewAdapter by lazy {
         BaseAdapter<ReviewTodoModel>(
@@ -68,16 +74,14 @@ class ReviewFragment  : BaseFragment<ReviewViewModel, FragmentReviewBinding>(),
         /**
          * Toolbar menu의 Item에 SearchView 위젯의 객체를 가져와 초기화
          */
-        mSearchView = toolbar.menu.findItem(R.id.search_menu_item)?.actionView as androidx.appcompat.widget.SearchView
         mSearchView.setOnQueryTextListener(this@ReviewFragment)
+        mSearchView.setOnQueryTextFocusChangeListener(this@ReviewFragment)
 
+        liveSharedPreferences.getString(BuildConfig.KEY_USER_NAME, null).observe(viewLifecycleOwner, Observer { displayName ->
+            LogUtil.i(Constants.TAG, "$THIS_NAME getString() called : $displayName")
 
-        liveSharedPreferences.getString(BuildConfig.KEY_USER_NAME, null)
-            .observe(viewLifecycleOwner, Observer { displayName ->
-                LogUtil.i(Constants.TAG, "$THIS_NAME getString() called : $displayName")
-
-                binding.displayName = displayName
-            })
+            binding.displayName = displayName
+        })
     }
 
 
@@ -100,15 +104,26 @@ class ReviewFragment  : BaseFragment<ReviewViewModel, FragmentReviewBinding>(),
         })
     }
 
+    /**
+     * SearchView의 Focus 상태(활성화, 비활성화)가 변경되면 호출되는 메서드
+     */
+    override fun onFocusChange(v: View?, hasFocus: Boolean) = when (hasFocus) {
+        true -> { Log.v(Constants.TAG, "$THIS_NAME - onFocusChange() SearchView On ")
+            binding.searchHistoryLayout.isVisible = true
+        }
+        false -> { Log.v(Constants.TAG, "$THIS_NAME - onFocusChange() SearchView Off")
+            binding.searchHistoryLayout.isInvisible = true
+        }
+    }
 
     /**
      * 키보드 검색버튼 클릭 시 SearchView 입력값을 가져오는 메서드
      */
     override fun onQueryTextSubmit(query: String?): Boolean {
-        Log.d(Constants.TAG, "onQueryTextSubmit() quary : $query")
+        Log.i(Constants.TAG, "onQueryTextSubmit() quary : $query")
 
-        this.mSearchView.setQuery("", false)    // SearchView의 입력값을 빈값으로 초기화
-        this.binding.toolbar.collapseActionView()   // 액션뷰가 닫힌다.
+        mSearchView.setQuery("", false)    // SearchView의 입력값을 빈값으로 초기화
+        binding.toolbar.collapseActionView()   // 액션뷰가 닫힌다.
 
         return true
     }
@@ -117,7 +132,7 @@ class ReviewFragment  : BaseFragment<ReviewViewModel, FragmentReviewBinding>(),
      * Toolbar의 SearchView 입력값이 변경되면 호출되는 메서드 (검색어 문자열 길이 제한)
      */
     override fun onQueryTextChange(newText: String?): Boolean {
-        Log.d(Constants.TAG, "$THIS_NAME onQueryTextChange() newText : $newText")
+        Log.i(Constants.TAG, "$THIS_NAME onQueryTextChange() newText : $newText")
 
         if (newText?.count() == 50) Toast.makeText(requireContext(), getString(R.string.String_length_limit), Toast.LENGTH_SHORT).show()
 
