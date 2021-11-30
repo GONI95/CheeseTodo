046bb36e7abc31c56dfc525378a61bf4505171ca
GONI95 <sanggoni009@gmail.com>
2021-11-30 13:56:55 +0900

    Feat : Splash Screen
    - drawable splash.xml 정의
    - styles.xml SplashTheme 정의
    - AndroidManifest.xml theme 적용
    application에 대한 theme를 SplashTheme로 적용한 후 MainActivity 외의 Activity들의 theme는 기본 테마로 적용
    
    Resolve:#30

--- app/src/main/java/sang/gondroid/cheesetodo/presentation/MainActivity.kt
+++ app/src/main/java/sang/gondroid/cheesetodo/presentation/MainActivity.kt
@@ -2,6 +2,8 @@ package sang.gondroid.cheesetodo.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -12,6 +14,7 @@ import sang.gondroid.cheesetodo.presentation.my.MyFragment
 import sang.gondroid.cheesetodo.presentation.review.ReviewFragment
 import sang.gondroid.cheesetodo.util.Constants
 import sang.gondroid.cheesetodo.util.LogUtil
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +23,44 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    var isReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        showContent()
         initViews()
+    }
+
+    /**
+     * Gon : android.R.id.content를 통해 Activity의 root view를 얻어와, ViewTree가 그려지는 것을 지연시키는 메서드
+     *       viewTreeObserver : ViewTree의 변경 사항을 알릴 수 있는 Listener를 등록할 때 사용
+     *       addOnPreDrawListener : ViewTree가 그려지려고 할 때 호출될 콜백을 등록
+     *       onPreDraw : ViewTree가 그려지려고 할 때 호출되는 콜백 메소드
+     *       [update - 21.11.30]
+     */
+    private fun showContent() {
+        thread(start=true) {
+            Thread.sleep(1000)
+            isReady = true
+        }
+
+        // Set up an OnPreDrawListener to the root view.
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                return if (isReady) {
+                    LogUtil.v(Constants.TAG, "$THIS_NAME, onPreDraw() true")
+                    // 이벤트 발생 후 제거해주지 않으면 콜백 메서드가 여러번 호출되는 현상이 발생
+                    content.viewTreeObserver.removeOnPreDrawListener(this)
+                    true
+                } else {
+                    LogUtil.v(Constants.TAG, "$THIS_NAME, onPreDraw() false")
+                    false
+                }
+            }
+        })
     }
 
     private fun initViews() = with(binding) {
